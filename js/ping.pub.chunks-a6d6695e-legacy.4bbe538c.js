3377ffde9bd42156d3370b9bb069a7b48811b2c7
zakir <nash@wokoworks.com>
2022-11-09 13:21:47 +0800

    init commit

---
+++ js/ping.pub.chunks-a6d6695e-legacy.4bbe538c.js
@@ -0,0 +1,8 @@
+"use strict";(self["webpackChunkping_wallet"]=self["webpackChunkping_wallet"]||[]).push([[4858],{7543:function(n,e,l){l.d(e,{GWp:function(){return u},kIL:function(){return a}});var t=l(99354),u=(0,t.D)("Blank",""),a=(0,t.D)("PersonFill",'<path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>');
+/*!
+ * BootstrapVue Icons, generated from Bootstrap Icons 1.2.1
+ *
+ * @link https://icons.getbootstrap.com/
+ * @license MIT
+ * https://github.com/twbs/icons/blob/master/LICENSE.md
+ */}}]);
